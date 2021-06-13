2f6b70aacd1c748b4d968b8170504327f1ee3eb0
makkareru <makkareru@gmail.com>
2021-06-14 03:37:35 +0900

    Fix ddl

--- sql/02_create_table.sql
+++ sql/02_create_table.sql
@@ -1,11 +1,11 @@
 USE testdb;
+SET FOREIGN_KEY_CHECKS = 0;
 
 DROP TABLE IF EXISTS user;
 CREATE TABLE user (
     user_id BIGINT NOT NULL PRIMARY KEY,
     name VARCHAR(20) NOT NULL,
-    register_datetime TIMESTAMP NOT NULL,
-    update_datetime TIMESTAMP NOT NULL
+    created_datetime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 DROP TABLE IF EXISTS user_profile;
@@ -13,9 +13,8 @@ CREATE TABLE user_profile (
     user_id BIGINT NOT NULL PRIMARY KEY,
     gender ENUM('MAN', 'WOMAN') NOT NULL,
     age INTEGER NOT NULL,
-    register_datetime TIMESTAMP NOT NULL,
-    update_datetime TIMESTAMP NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user(user_id)
+    created_datetime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY user_profile_user_id_foreign_key (user_id) REFERENCES user(user_id)
 );
 
 DROP TABLE IF EXISTS user_password;
@@ -23,7 +22,8 @@ CREATE TABLE user_password (
     user_id BIGINT NOT NULL PRIMARY KEY,
     password VARBINARY(255) NOT NULL,
     solt VARCHAR(32) NOT NULL,
-    register_datetime TIMESTAMP NOT NULL,
-    update_datetime TIMESTAMP NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user(user_id)
+    created_datetime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY user_password_user_id_foreign_key (user_id) REFERENCES user (user_id)
 );
+
+SET FOREIGN_KEY_CHECKS = 1;
